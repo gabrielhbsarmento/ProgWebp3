a36f66ecdbaa5db9995b5f0cf8c0129c182cf61d
Gabriel Henrique Baracat Sarmento <88384123+gabrielhbsarmento@users.noreply.github.com>
2021-11-30 22:54:19 -0300

    Etapa 5

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,19 @@ import React, {useState} from 'react';
 import API from './api'; 
 
 function App() {
+
+    /**function getById(){
+        const id = document.querySelector('#user').value
+        axios.get(`http://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(res => showResponse(res))
+    }/** */
+
+    API.get('/posts')
+    .then(etapa6 => console.log(etapa6))
+    /**function post(){
+        const title= document.querySelector('#post').value
+        axios.post("http://jsonplaceholder.typicode.com/posts",{title}).then(res => showResponse(res))
+    }/** */
     return <div>Comece aqui!</div>;
 }
 
